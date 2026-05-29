74d402ff8ae5fbc875012f48d3ac0a61980518b0
manaman-z <zhamalidinovspain@gmail.com>
2026-05-29 13:40:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js, { scope: '/visual-music/' }")
       .then((reg) =>
         console.log("Service Worker Registered Successfully!", reg)
       )
